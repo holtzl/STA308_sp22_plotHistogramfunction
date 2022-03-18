0d633fc539019d63db216767fee1c00c67e7c4dc
holtzl <holtzl@miamioh.edu>
2022-03-18 10:59:43 -0400

    Added the function and testing code on march 18.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # STA308_sp22_plotHistogramfunction
+
 A function that generates some data and makes a histogram
+
+## update on friday march 18
+
+add the base fucntion that we will use again after spring break
+
+
---
+++ plotrandomfunction.R
@@ -0,0 +1,29 @@
+##############################
+##
+## plotrandomfunction.R
+##
+## Zach Holt
+##March 18, 2022
+##
+## A function that generates a sequence of random
+## normal variables and makes a histogram
+##
+##Friday, March 11, 2022
+##
+## The function expects the following inputs
+## n- sample size (default with n=1)
+## mu- mean of normal distribution (default, mu=0)
+## signma- sd of normal distribution (default, sigma=1)
+## hist_breaks- number of break points in histogram (default 6)
+##
+# plotRandom <- function(n=1) {
+#   hist(rnorm(n=n, mean=0, sd=1))
+# }
+
+## today we will add to it
+plotRandom <- function(n=1, mu=0, sigma=1, hist_breaks=6) {
+  hist(rnorm(n=n, mean=mu, sd=sigma),
+       breaks=hist_breaks)
+}
+
+
